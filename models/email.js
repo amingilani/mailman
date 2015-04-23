a044d389002c20b1fcd7402d5602aa6834011572
Amin Shah Gilani <aminshahgilani@gmail.com>
2015-04-23 09:34:51 +0500

    add callback, to, from, and type fields to mailSchema

--- models/email.js
+++ models/email.js
@@ -3,10 +3,16 @@ var bcrypt   = require('bcrypt-nodejs');
 
 // define the schema for our user model
 var emailSchema = mongoose.Schema ({
+  type                :   String, // "reward" for cced mails or "incoming"
+  incomingEmail       :   String, // user_id where user_id@mailman.ninja
+  to                  :   String,
+  date                :   Date,
+  cc                  :   String,
   recipient           : 	String,
   sender              : 	String,
   from                : 	String,
   subject             : 	String,
+  subjectStripped     :   String, // subject with the "re", "fwd" stripped
   bodyPlain           : 	String,
   strippedText        : 	String,
   strippedSignature   : 	String,
@@ -16,6 +22,7 @@ var emailSchema = mongoose.Schema ({
   attachmentx         : 	String,
   messageHeaders      : 	String,
   contentIdMap        : 	String,
-  btcAddress          :   String
+  btcAddress          :   String, // generated address for the email
+  callbackRes         :   Object // the callback when address is paid
 });
 module.exports = mongoose.model('Email', emailSchema);
