3847ae443b0fdb8dbfcc4f4c82f92d5e0643075b
Amin Shah Gilani <aminshahgilani@gmail.com>
2015-04-23 13:05:54 +0500

    secured coinbase callbacks with jwt, and beautified

--- routes/api.js
+++ routes/api.js
@@ -2,23 +2,29 @@
 //////////////////
 // Config
 var config = require('./config/config'),
-// Express
-    express = require('express'),
-    router = express.Router(),
-// the Mail Model
-    Mail = require('../models/email.js'),
-// Coinbase
-    Client = require('coinbase').Client,
-    client = new Client({
-      'apiKey'    : config.coinbase.testnet.key,
-      'apiSecret' : config.coinbase.testnet.secret,
-      'baseApiUri': 'https://api.sandbox.coinbase.com/v1/'
-      }),
-    Account   = require('coinbase').model.Account,
-    btcAccount = new Account(client, {'id': '55335c04fb9854796c00000c'}),
-// Mailgun
-    Mailgun = require('mailgun').Mailgun,
-    mg = new Mailgun(config.mailgun);
+  secret = config.secret,
+  // Express
+  express = require('express'),
+  router = express.Router(),
+  // the Mail Model
+  Mail = require('../models/email.js'),
+  // Coinbase
+  Client = require('coinbase').Client,
+  client = new Client({
+    'apiKey': config.coinbase.testnet.key,
+    'apiSecret': config.coinbase.testnet.secret,
+    'baseApiUri': 'https://api.sandbox.coinbase.com/v1/'
+  }),
+  Account = require('coinbase').model.Account,
+  btcAccount = new Account(client, {
+    'id': '55335c04fb9854796c00000c'
+  }),
+  // Mailgun
+  Mailgun = require('mailgun').Mailgun,
+  mg = new Mailgun(config.mailgun),
+
+  // JSON Web Tokens
+  jwt = require('jsonwebtoken');
 
 /* API ROUTES */
 
@@ -30,42 +36,44 @@ router.post('/mailman', function(req, res) {
 
     Mail.findOne({
       subjectStripped: req.body.subject.replace(/([\[\(] *)?(RE|FWD?) *([-:;)\]][ :;\])-]*|$)|\]+ *$/igm, "")
-    }, function (err, mail) {
+    }, function(err, mail) {
       if (err) {
         console.log(err);
       } else if (mail && (mail.to === req.body.from &&
-        mail.from === req.body.to)) {
+          mail.from === req.body.to)) {
         // if the mail exists and is being sent back from the original sender
+
         // TODO pay the man
+        // LOTSA CODE
 
 
       } else if (!mail) {
         // if no such mail exists
 
-    // save the metadata
-    mail = new Mail();
-    mail.type = "reward";
-    mail.to = req.body.to;
-    mail.date = req.body.Date;
-    mail.cc = req.body.Cc;
-    mail.sender = req.body.sender;
-    mail.from = req.body.from;
-    mail.subject = req.body.subject;
+        // save the metadata
+        mail = new Mail();
+        mail.type = "reward";
+        mail.to = req.body.to;
+        mail.date = req.body.Date;
+        mail.cc = req.body.Cc;
+        mail.sender = req.body.sender;
+        mail.from = req.body.from;
+        mail.subject = req.body.subject;
 
-    // regex expression to find "re:", "fw:" "fwd:", etc.
-    var junkRegex = /([\[\(] *)?(RE|FWD?) *([-:;)\]][ :;\])-]*|$)|\]+ *$/igm;
-    // strip all re, and fwd from subject before saving as the stripped subject
-    mail.subjectStripped = req.body.subject.replace(junkRegex, "");
+        // regex expression to find "re:", "fw:" "fwd:", etc.
+        var junkRegex = /([\[\(] *)?(RE|FWD?) *([-:;)\]][ :;\])-]*|$)|\]+ *$/igm;
+        // strip all re, and fwd from subject before saving as the stripped subject
+        mail.subjectStripped = req.body.subject.replace(junkRegex, "");
 
-    //create the address
-    btcAccount.createAddress({
-      "callback_url": 'http://mailman.ninja/api/payment/' + mail.id,
-      "label": ""
-      }, function(err, address) {
-        if (err) {
-          // output error and save mail
-          console.log(err);
-          mail.save();
+        //create the address
+        btcAccount.createAddress({
+          "callback_url": 'http://mailman.ninja/api/payment/' + mail.id,
+          "label": ""
+        }, function(err, address) {
+          if (err) {
+            // output error and save mail
+            console.log(err);
+            mail.save();
           } else {
             // save the address and save the mail
             mail.btcAddress = address.address;
@@ -78,66 +86,75 @@ router.post('/mailman', function(req, res) {
               'noreply@mailman.ninja', {},
               function(err) {
                 if (err) console.log('Unable to deliver invoice for mail ' +
-                mail.id + '\nerror: ' + err);
-                else     console.log('Success');
-            });
+                  mail.id + '\nerror: ' + err);
+                else console.log('Success');
+              });
           }
         });
-        // for all over errors
       }
-    }
-  );
-}});
+    });
+  }
+});
 
 // When a callback is recieved for a payment made.
 // '/payment/:mail_id'
 router.post('/payment/:mail_id', function(req, res) {
 
-/* Example object to be recieved
-
-  {
-  "address": "1AmB4bxKGvozGcZnSSVJoM6Q56EBhzMiQ5",
-  "amount": 1.23456,
-  "transaction": {
-    "hash": "7b95769dce68b9aa84e4aeda8d448e6cc17695a63cc2d361318eb0f6efdf8f82"
-  }
-*/
-
-  // find the mail with this id
-  Mail.findById(req.params.mail_id, function (err, mail) {
+  jwt.verify(token, secret, function(err, decoded) {
     if (err) {
-      console.log(err);
-    } else {
-      // save the callback object in the db
-      mail.callbackRes = req.body;
-      mail.save();
-
-// TODO determine what sort of mail this was
-
-    if (mail.type === 'reward') {
-
-      // mail the person saying there is a reward available
-      mg.sendText('Mailman <mailman@mailman.ninja>', [mail.to],
-        'RE: ' + mail.subject,
-        'Hi, there\'s a ' + req.body.amount +
-        'reward on replying to this email.\n ' +
-        'Just keep `mailman@mailman.ninja` in the CC field so that I know you\'ve replied!',
-        'noreply@mailman.ninja', {},
-        function(err) {
-          if (err) console.log('Unable to deliver invoice for mail ' +
-          mail.id + '\nerror: ' + err);
-          else     console.log('Success');
+      return res.status(403).send({
+        success: false,
+        message: 'Invalid token'
       });
-    } else if (mail.type === 'incoming') {
+    } else if (decoded) {
+      req.decoded = decoded;
+      /* Example object to be recieved
 
-      // TODO deliver the email to its rightful recepient
-    }
+        {
+        "address": "1AmB4bxKGvozGcZnSSVJoM6Q56EBhzMiQ5",
+        "amount": 1.23456,
+        "transaction": {
+          "hash": "7b95769dce68b9aa84e4aeda8d448e6cc17695a63cc2d361318eb0f6efdf8f82"
+        }
+      */
+
+      // find the mail with this id
+      Mail.findById(req.params.mail_id, function(err, mail) {
+        if (err) {
+          console.log(err);
+        } else {
+          // save the callback object in the db
+          mail.callbackRes = req.body;
+          mail.save();
+
+          // TODO determine what sort of mail this was
+
+          if (mail.type === 'reward') {
+
+            // mail the person saying there is a reward available
+            mg.sendText('Mailman <mailman@mailman.ninja>', [mail.to],
+              'RE: ' + mail.subject,
+              'Hi, there\'s a ' + req.body.amount +
+              'reward on replying to this email.\n ' +
+              'Just keep `mailman@mailman.ninja` in the CC field so that I know you\'ve replied!',
+              'noreply@mailman.ninja', {},
+              function(err) {
+                if (err) console.log('Unable to deliver invoice for mail ' +
+                  mail.id + '\nerror: ' + err);
+                else console.log('Success');
+              });
+          } else if (mail.type === 'incoming') {
+
+            // TODO deliver the email to its rightful recepient
+          }
+        }
+      });
     }
   });
 });
 
 // new mail to specifc user
-router.post('/mail/:user_id', function (req, res){
+router.post('/mail/:user_id', function(req, res) {
   console.log(req.body.lol);
   res.send(req.body['lol-hello']);
 
@@ -162,53 +179,60 @@ router.post('/mail/:user_id', function (req, res){
   mail.messageHeaders = req.body['message-headers'];
   mail.contentIdMap = req.body['content-id-map'];
 
-  btcAccount.createAddress({
-    "callback_url": 'http://mailman.ninja/api/payment/' + mail.id,
-    "label": ""
-    }, function(err, address) {
-      if (err) {
-        // output error and save mail
-        console.log(err);
-        mail.save();
-        } else {
-          // save the address and save the mail
-          mail.btcAddress = address.address;
-          mail.save();
+  var callbackToken = jwt.sign({
+    'mail_id': mail.id,
+  }, secret);
 
-          // send an invoice to the sender
-          mg.sendText('Mailman <mailman@mailman.ninja>', [mail.sender],
-            'RE: ' + mail.subject,
-            'Hi, your email will only be delivered if you pay for its delivery.\n' +
-            'Please pay at this address: ' + mail.btcAddress,
-            'noreply@mailman.ninja', {},
-            function(err) {
-              if (err) console.log('Unable to deliver invoice for mail ' +
-              mail.id + '\nerror: ' + err);
-              else     console.log('Success');
-          });
-        }
-      });
+  var addressArgs = {
+    'callback_url': 'http://mailman.ninja/api/payment/' + mail.id +
+      '?token=' + callbackToken,
+    'label': mail.id
+  };
+
+  btcAccount.createAddress(addressArgs, function(err, address) {
+    if (err) {
+      // output error and save mail
+      console.log(err);
+      mail.save();
+    } else {
+      // save the address and save the mail
+      mail.btcAddress = address.address;
+      mail.save();
+
+      // send an invoice to the sender
+      mg.sendText('Mailman <mailman@mailman.ninja>', [mail.sender],
+        'RE: ' + mail.subject,
+        'Hi, your email will only be delivered if you pay for its delivery.\n' +
+        'Please pay at this address: ' + mail.btcAddress,
+        'noreply@mailman.ninja', {},
+        function(err) {
+          if (err) console.log('Unable to deliver invoice for mail ' +
+            mail.id + '\nerror: ' + err);
+          else console.log('Success');
+        });
+    }
+  });
 
 });
 
 // User authorization (login)
 router.post('/user/auth', passport.authenticate('local-login', {
-    successRedirect : '/user', // redirect to the user
-    failureRedirect : '/', // redirect back to the home page on error
-    failureFlash : true // allow flash messages
+  successRedirect: '/user', // redirect to the user
+  failureRedirect: '/', // redirect back to the home page on error
+  failureFlash: true // allow flash messages
 }));
 
 // User signup
 router.post('/user/new', passport.authenticate('local-signup', {
-    successRedirect : '/profile', // redirect to the secure profile section
-    failureRedirect : '/signup', // redirect back to the signup page if there is an error
-    failureFlash : true // allow flash messages
+  successRedirect: '/profile', // redirect to the secure profile section
+  failureRedirect: '/signup', // redirect back to the signup page if there is an error
+  failureFlash: true // allow flash messages
 }));
 
 // User logout
-router.get('/user/lsogout', function(req, res) {
-    req.logout();
-    res.redirect('/');
+router.get('/user/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
 });
 
 
