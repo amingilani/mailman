9c31413ef0dc4479562ece46a9b726f8f0944177
Amin Shah Gilani <aminshahgilani@gmail.com>
2015-05-08 17:19:46 +0500

    more debug

--- routes/api.js
+++ routes/api.js
@@ -152,7 +152,9 @@ module.exports = function(app, passport) {
         "hash": "7b95769dce68b9aa84e4aeda8d448e6cc17695a63cc2d361318eb0f6efdf8f82"
       }
     */
-    console.log('Recieved a payment notification'); //debug
+    console.log('Recieved a payment notification with the following token'); //debug
+    console.log(req.param.token);
+
     jwt.verify(req.param.token, secret, function(err, decoded) {
       if (err) {
         console.log(err);
