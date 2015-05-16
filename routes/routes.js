1f4c3f85b770094ab7562782a36eccb71dceeb47
Amin Shah Gilani <aminshahgilani@gmail.com>
2015-05-17 04:07:50 +0500

    plugged in the new interface

--- routes/routes.js
+++ routes/routes.js
@@ -5,9 +5,7 @@ module.exports = function(app, passport) {
     // =====================================
     // HOME PAGE (with login links) ========
     // =====================================
-    app.get('/', function(req, res) {
-        res.render('index.ejs'); // load the index.ejs file
-    });
+    app.get('/', express.static('index.html'));
 
     // =====================================
     // LOGIN ===============================
