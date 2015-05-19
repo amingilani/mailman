223b5d3f2d083ee144fa082129778723fdf5d433
Amin Shah Gilani <aminshahgilani@gmail.com>
2015-05-19 18:30:21 +0500

    moved config out of code

--- config/config.js
+++ config/config.js
@@ -1,43 +1,13 @@
-module.exports = {
-  development : true,
-  secret      : "0A8WBeBzLQy4qktXwTGyqZoOahX0tCyKR0bfjO2WqNWKlvyf0O4eiNwXtJ" +
-                "2Mpppcc6XIRSDoKocTkW2buV0sRYyXHWB6nq4dn9OHeKilPJOR46kcIF2I" +
-                "Bt02ZjGXx7Z",
-  coinbase    : {
-          /* TODO
-          key   : function () {
-            if (development) {
-              return this.testnet.key;
-            } else {
-              return this.livenet.key;
-            }
-          },
-          secret: function () {
-            if (development) {
-              return this.testnet.secret
-            } else {
-              return this.livenet.secret
-            }
-          },
-          */
-          livenet      : {
-                  key     : "UCQ4Kuw1NotbNlCX",
-                  secret  : "ygjISE1RKvT82hD2aRezy2j86SP5q2WM"
-      },
-          testnet     : {
-                  key       : "S8scckTGBWeAXjht",
-                  secret    : "VYOPdjUgBKQuzMKv3w5kz5KhvLOlUXpq"
-      }
-  },
-  db          : {
-          user : "app",
-          pass : "pNwySu3Mo3vh5ZKUJlasMJ2ZnnffFUeFoG75eA02SobPoxQruTCM7mdmd",
-          host : "ds061661.mongolab.com:61661/mail-ninja",
-          url :  function () {
-            return "mongodb://" + this.user + ":" + this.pass + "@" + this.host;
-            }
+var config = {};
 
-  },
-  mailgun     : "key-b9d0baf1ac20ef3056c83731bc0abe5a"
+config.coinbase.accId ="process.env.coinbase_acc_id";
+config.coinbase.apiKey ="process.env.coinbase_api_key";
+config.coinbase.apiSec ="process.env.coinbase_api_secret";
+config.coinbase.apiUrl ="process.env.coinbase_api_url";
+config.dbHost ="process.env.db_host";
+config.dbPass ="process.env.db_pass";
+config.dbUser ="process.env.db_user";
+config.mailgun.apiKey ="process.env.mailgun_api_key";
+config.secret ="process.env.secret";
 
-};
+module.exports = config;
