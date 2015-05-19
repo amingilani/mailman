d3f83f0f5072322342ee69dcea82b34589b06cb4
Amin Shah Gilani <aminshahgilani@gmail.com>
2015-05-19 18:51:53 +0500

    define nested objected before definig their properties

--- config/config.js
+++ config/config.js
@@ -4,15 +4,18 @@ var config = {};
 config.secret = process.env.secret;
 
 // Mailgun config
+config.mailgun = {};
 config.mailgun.apiKey = process.env.mailgun_api_key;
 
 // Coinbase config
+config.coinbase = {};
 config.coinbase.accId = process.env.coinbase_acc_id;
 config.coinbase.apiKey = process.env.coinbase_api_key;
 config.coinbase.apiSec = process.env.coinbase_api_secret;
 config.coinbase.apiUrl = process.env.coinbase_api_url;
 
 // DB config
+config.db = {};
 config.db.host = process.env.db_host;
 config.db.pass = process.env.db_pass;
 config.db.user = process.env.db_user;
