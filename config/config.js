d03f13178f8a9fa812ae32278a01714459695f43
Amin Shah Gilani <aminshahgilani@gmail.com>
2015-05-19 18:39:39 +0500

    reformatted and completed dbconfig code

--- config/config.js
+++ config/config.js
@@ -1,13 +1,23 @@
 var config = {};
 
+// Mailman config
+config.secret ="process.env.secret";
+
+// Mailgun config
+config.mailgun.apiKey ="process.env.mailgun_api_key";
+
+// Coinbase config
 config.coinbase.accId ="process.env.coinbase_acc_id";
 config.coinbase.apiKey ="process.env.coinbase_api_key";
 config.coinbase.apiSec ="process.env.coinbase_api_secret";
 config.coinbase.apiUrl ="process.env.coinbase_api_url";
-config.dbHost ="process.env.db_host";
-config.dbPass ="process.env.db_pass";
-config.dbUser ="process.env.db_user";
-config.mailgun.apiKey ="process.env.mailgun_api_key";
-config.secret ="process.env.secret";
+
+// DB config
+config.db.host ="process.env.db_host";
+config.db.pass ="process.env.db_pass";
+config.db.user ="process.env.db_user";
+config.db.url = function () {
+            return "mongodb://" + this.user + ":" + this.pass + "@" + this.host;
+          };
 
 module.exports = config;
