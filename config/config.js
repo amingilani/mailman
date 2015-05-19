c90eb49ad6f0b318869dc2c31009be53bca94325
Amin Shah Gilani <aminshahgilani@gmail.com>
2015-05-19 18:49:34 +0500

    whoops, accidentally added quotation marks

--- config/config.js
+++ config/config.js
@@ -1,21 +1,21 @@
 var config = {};
 
 // Mailman config
-config.secret ="process.env.secret";
+config.secret = process.env.secret;
 
 // Mailgun config
-config.mailgun.apiKey ="process.env.mailgun_api_key";
+config.mailgun.apiKey = process.env.mailgun_api_key;
 
 // Coinbase config
-config.coinbase.accId ="process.env.coinbase_acc_id";
-config.coinbase.apiKey ="process.env.coinbase_api_key";
-config.coinbase.apiSec ="process.env.coinbase_api_secret";
-config.coinbase.apiUrl ="process.env.coinbase_api_url";
+config.coinbase.accId = process.env.coinbase_acc_id;
+config.coinbase.apiKey = process.env.coinbase_api_key;
+config.coinbase.apiSec = process.env.coinbase_api_secret;
+config.coinbase.apiUrl = process.env.coinbase_api_url;
 
 // DB config
-config.db.host ="process.env.db_host";
-config.db.pass ="process.env.db_pass";
-config.db.user ="process.env.db_user";
+config.db.host = process.env.db_host;
+config.db.pass = process.env.db_pass;
+config.db.user = process.env.db_user;
 config.db.url = function () {
             return "mongodb://" + this.user + ":" + this.pass + "@" + this.host;
           };
